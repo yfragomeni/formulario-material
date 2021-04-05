227f4fd11c47399188934f070861e1aeabab09f0
yfragomeni <yfragomeni@gmail.com>
2021-04-05 00:41:02 -0300

    aula 3

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,28 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, TextField, Switch, FormControlLabel } from '@material-ui/core';
 
 function FormularioCadastro() {
+    const [nome, setNome] = useState("");
+    const [sobrenome, setSobrenome] = useState("");
+    const [cpf, setCpf] = useState("");
+
     return(
-        <form>
-            <TextField 
+        <form
+        onSubmit={(event) => 
+            {event.preventDefault();
+            console.log(nome, sobrenome, cpf);}
+        }>
+            <TextField
+            value={nome} 
             variant="outlined" 
             label="Nome"
             id="nome"
             margin="normal"
-            fullWidth/>
+            fullWidth
+            onChange={(event) => {
+                let nomeTemp = event.target.value;
+                if(nomeTemp.length >= 3) {
+                    nomeTemp = nomeTemp.substr(0,3);
+                }
+                setNome(nomeTemp);
+            }}/>
 
             <TextField 
+            value={sobrenome}
             variant="outlined" 
             label="Sobrenome"
             id="sobrenome"
             margin="normal"
+            onChange = {(event) => {
+                setSobrenome(event.target.value);
+            }
+            }
             fullWidth/>
 
-            <TextField 
+            <TextField
+            value={cpf} 
             variant="outlined" 
             label="CPF"
             id="cpf"
             margin="normal"
+            onChange = {(event) => {
+                setCpf(event.target.value);
+            }
+            }
             fullWidth/>
 
             <FormControlLabel
@@ -42,6 +68,7 @@ function FormularioCadastro() {
             label="Novidades"/>
 
             <Button 
+            type="submit"
             variant="contained"
             color="primary">Cadastrar</Button>
         </form>
